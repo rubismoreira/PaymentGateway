e86467bed7616023ee08112a11f2b6d3665a46b2
Rubem Moreira <rubem.moreira@sage.com>
2020-12-13 21:21:44 +0100

    Create bank client

---
+++ CO.PaymentGateway/CO.PaymentGateway.BankClient/Client/BankHttpClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CO.PaymentGateway.BankClient.Entities;
+
+namespace CO.PaymentGateway.BankClient.Client
+{
+    public class BankHttpClient : IBankHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _remoteServiceBaseUrl;
+
+        public BankHttpClient(HttpClient client)
+        {
+            this._httpClient = client;
+        }
+
+        public async Task<BankResponse> CreatePayment(BankPayment payment)
+        {
+            try
+            {
+                HttpResponseMessage responseMessage = await _httpClient.PostAsJsonAsync("BankPayment", payment);
+
+                responseMessage.EnsureSuccessStatusCode();
+
+                var bankResponse = await responseMessage.Content.ReadAsAsync<BankResponse>();
+
+                return bankResponse;
+            }
+            catch (Exception ex)
+            {
+                return new BankResponse { BankResponseId = Guid.Empty };
+            }
+        }
+
+    }
+}
