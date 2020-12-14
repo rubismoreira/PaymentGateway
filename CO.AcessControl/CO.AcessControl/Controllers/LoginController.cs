3cb22441a911907f4ab9118964cff2adad69f7ba
Rubem Moreira <rubismoreira@gmail.com>
2020-12-14 17:17:06 +0100

    Create Access Service

---
+++ CO.AcessControl/CO.AcessControl/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using CO.AcessControl.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CO.AcessControl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private List<User> appUsers = new List<User>
+        {
+            new User { Username = "UserRead", Password = "1234", Roles = new List<string>{ "ReadProcessPayment"} },
+            new User { Username = "UserWrite", Password = "1234", Roles = new List<string> { "WriteProcessPayment" } },
+            new User { Username = "UserFull", Password = "1234", Roles = new List<string> { "ReadProcessPayment", "WriteProcessPayment" } },
+            new User { Username = "UserNone", Password = "1234", Roles = new List<string> {  } }
+         };
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] User login)
+        {
+            IActionResult response = Unauthorized();
+            User user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJWTToken(user);
+                response = Ok(new
+                {
+                    token = tokenString,
+                    userDetails = user,
+                });
+            }
+            return response;
+        }
+        User AuthenticateUser(User loginCredentials)
+        {
+            User user = appUsers.SingleOrDefault(x => x.Username == loginCredentials.Username && x.Password == loginCredentials.Password);
+            return user;
+        }
+        string GenerateJWTToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt: SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.Username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("role", userInfo.Roles.FirstOrDefault());
+            };
+
+
+            var token = new JwtSecurityToken(
+            issuer: _config["Jwt: Issuer"],
+            audience: _config["Jwt: Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddDays(30),
+            signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
+
---
+++ CO.AcessControl/CO.AcessControl/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CO.AcessControl.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public List<string> Roles { get; set; }
+    }
+}
